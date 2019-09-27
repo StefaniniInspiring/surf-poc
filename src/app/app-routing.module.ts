e2c3444e2bab99cc3af1e78c5243893003bc39a1
leonavas <rodrigoleonavas@gmail.com>
2019-09-27 05:17:25 -0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home' , component: HomeComponent
+  }
+  // {
+  //   path: 'home',
+  //   loadChildren: () =>
+  //     import('./pages/home/home.module').then(m => m.HomeModule) // new dynamic import method
+  // }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
